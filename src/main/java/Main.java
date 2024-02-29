574f4c9b12493ad6b0a8884c79d1bf05512bdb2d
deepa-kakade-git <deep_303@yahoo.com>
2024-02-29 18:00:11 -0500

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,47 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
-public class Main {
+public class Main
+{
 
     public static void main(String[] args ){
         // create a new object of class Main
+        Main main = new Main();
+
 
         // call the compute() method on that new object
+        compute();
     }
 
-    public void compute() {
+    public static void compute() {
+        String name;
+        Scanner scanner = new Scanner(System.in);
         // Get input string of name from user
-        
-        // Create conditional to check name against Alice and Bob
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
 
-        // Print greeting to screen if Alice or Bob are true
-        
-        // Print "You are neither Alice nor Bob." otherwise
 
+        if (name.equals("Alice")  || name.equals("Bob")){
+            System.out.println("Hello "+ name);
+        }
+        else {
+            System.out.println("You are neither Alice nor Bob");
+
+        }
     }
+
+
+
+
+
+
+    // Create conditional to check name against Alice and Bob
+
+    // Print greeting to screen if Alice or Bob are true
+
+    // Print "You are neither Alice nor Bob." otherwise
+
+
 }
